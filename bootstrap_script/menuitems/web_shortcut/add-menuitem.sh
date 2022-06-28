828303746ae29ccd977f5df2f7e4beb47e9a35f6
CiLunZeng <cilun0812@gmail.com>
2022-06-28 22:42:16 +0800

    Complete the web_shortcut script.

--- bootstrap_script/menuitems/web_shortcut/add-menuitem.sh
+++ bootstrap_script/menuitems/web_shortcut/add-menuitem.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# refer to https://unix.stackexchange.com/questions/585997/assign-an-icon-to-a-custom-mimetype
+#          https://askubuntu.com/questions/1238924/assign-an-icon-to-a-custom-mimetype
 
 ICON_NAME=application-x-mswinurl
 TMP_DIR=`mktemp --directory` 
@@ -24,13 +26,18 @@ EOF
 # seems necessary to refresh immediately:
 chmod 644 $DESKTOP_FILE
 
+# Note: Maybe need add this syntax to /etc/mime.types?
+# application/x-mswinurl          		url 
 xdg-desktop-menu install $DESKTOP_FILE
+xdg-mime install --mode user chromeopen.xml
 xdg-icon-resource install --context mimetypes --size 16 "${ICON_NAME}_16.png" $ICON_NAME
 xdg-icon-resource install --context mimetypes --size 24 "${ICON_NAME}_24.png" $ICON_NAME
 xdg-icon-resource install --context mimetypes --size 48 "${ICON_NAME}_48.png" $ICON_NAME
 xdg-icon-resource install --context mimetypes --size 64 "${ICON_NAME}_64.png" $ICON_NAME
 xdg-icon-resource install --context mimetypes --size 128 "${ICON_NAME}_128.png" $ICON_NAME
 xdg-icon-resource install --context mimetypes --size 256 "${ICON_NAME}_256.png" $ICON_NAME
+update-mime-database ~/.local/share/mime
+update-icon-caches ~/.local/share/icons
 
 rm $DESKTOP_FILE
 rm -R $TMP_DIR
--- bootstrap_script/menuitems/web_shortcut/remove-menuitem.sh
+++ bootstrap_script/menuitems/web_shortcut/remove-menuitem.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 xdg-desktop-menu uninstall chromeopen.desktop
+xdg-mime uninstall --mode user chromeopen.xml
 xdg-icon-resource uninstall --context mimetypes --size 16 application-x-mswinurl
 xdg-icon-resource uninstall --context mimetypes --size 24 application-x-mswinurl
 xdg-icon-resource uninstall --context mimetypes --size 48 application-x-mswinurl
 xdg-icon-resource uninstall --context mimetypes --size 64 application-x-mswinurl
 xdg-icon-resource uninstall --context mimetypes --size 128 application-x-mswinurl
 xdg-icon-resource uninstall --context mimetypes --size 256 application-x-mswinurl
+update-mime-database ~/.local/share/mime
+update-icon-caches ~/.local/share/icons
